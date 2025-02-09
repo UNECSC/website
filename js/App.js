7b279219adb2be3371ab1286ac5d55ee867740cb
Cody Williams <cwilli87@myune.edu.au>
2025-02-09 21:35:10 +1100

    Fixed content z index

--- js/App.js
+++ js/App.js
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 import Events from './pages/Events';
 import Signup from './pages/Signup';
 import Contact from './pages/Contact';
-import Highscores from './pages/Highscores'; 
+import Highscores from './pages/Highscores';
 import Content from './Content';
 import AnimatedBG from './components/AnimatedBG';
 
@@ -17,30 +17,33 @@ function App() {
     setContentKey((prevKey) => prevKey + 1);
     setTimeout(() => {
       callback();
-    }, 500); // Duration matches the CSS transition duration
+    }, 500); // Matches CSS transition duration
   };
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-      {/* Animated Background */}
-      <AnimatedBG />
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+      {/* Background Wrapper - Always behind content */}
+      <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: -100 }}>
+        <AnimatedBG />
+      </div>
 
-      <Router>
-        {/* Navbar and dropdown menu */}
-        <Navbar />
-        <DropdownMenu onChangeContent={handleChangeContent} />
+      {/* Main Content Wrapper - Ensures UI is above background */}
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <Router>
+          <Navbar />
+          <DropdownMenu onChangeContent={handleChangeContent} />
 
-        {/* Main content area */}
-        <Content key={contentKey}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/highscores" element={<Highscores />} />
-          </Routes>
-        </Content>
-      </Router>
+          <Content key={contentKey}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/highscores" element={<Highscores />} />
+            </Routes>
+          </Content>
+        </Router>
+      </div>
     </div>
   );
 }
